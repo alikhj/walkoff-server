60be63f257fbe20894d7fd499113947f9e3ac5a4
Ali Khawaja <alikhj@gmail.com>
2015-08-15 01:34:19 -0400

    more db

--- server.js
+++ server.js
@@ -46,33 +46,27 @@ function createDatabase() {
       })
     }
     walkoff = rethink.db('walkoff')
-    createTable()
+    createTables()
   })
 }
 
-function createTable() {
+function createTables() {
   walkoff.tableList().run(connection, function(err, tables) {
     if(err) {
       throw new Error('error getting the list of databases: ', err)
     }
-
-    if(tables.indexOf('games') === -1) {
-      walkoff.tableCreate('games').run(connection, function(err, response) {
-        if(err) {
-          throw new Error('there was a problem creating the games table: ', err)
-        }
-        console.log('created games table')
-      })
-    }
-
-    if(tables.indexOf('players') === -1) {
-      walkoff.tableCreate('players').run(connection, function(err, response) {
-        if(err) {
-          throw new Error('there was a problem creating the games table: ', err)
-        }
-        console.log('created players table')
-      })
-    }
+    createTableWithName('games')
+    createTableWithName('players')
+    function createTableWithName(tableName) {
+      if(tables.indexOf(tableName) === -1) {
+        walkoff.tableCreate(tableName).run(connection, function(err, response) {
+          if(err) {
+            throw new Error('error creating table with name: ' + tableName)
+          }
+          console.log('table created with name: ' + tableName)
+        })
+      }
+    }   
   })
 }
 
@@ -82,10 +76,57 @@ io.on('connection', function (socket) {
   socket.games = []
   var playerData = {
     connected: true,
-    sid: socket.id, 
     score: '',
     lastUpdated: ''
   }
+
+  socket.on('player-connected', function (data) {
+    walkoff.table('players').get(data.playerID).update({
+      sid: socket.id,
+      lastUpdated: rethink.now()
+    }).run(connection, function(err, response) {
+      console.log('\n' + getTimeStamp() + ' ' + data.playerID +
+		  ' updated with new sid in players table: ' +  
+		  '\n\t sid: ' + socket.id)
+     if (response.skipped == 1) {
+        walkoff.table('players').insert({
+          id: data.playerID,
+          sid: socket.id,
+	  lastUpdated: rethink.now()
+	}).run(connection, function(err, response){
+	  console.log('\n' + getTimeStamp() + data.playerID +
+		      ' does not exist, adding to players table:' +
+		      '\n\t id: ' + data.playerID +
+		      '\n\t sid: ' + socket.id)  
+	})
+      }
+    })
+  })
+ 
+  socket.on('disconnect', function() {
+    console.log('test: ' + socket.id)
+    walkoff.table('players').filter({sid: socket.id}).
+    run(connection, function(err, cursor) {
+      cursor.toArray(function(err, results) {
+
+        if(results.length) {
+	  console.log('results: ' + results[0].sid)
+	}
+      })
+    })
+
+    console.log('\n' + getTimeStamp() + ' player disconnected (this message could be delayed)' +
+                '\n\t sid: ' + socket.id +
+                '\n\t playerID: ' + socket.playerID +
+                '\n\t leaving games:')
+    for (game in socket.games) {
+      console.log('\t' + socket.games[game])
+      socket.to(socket.games[game]).emit('player-disconnected', {
+          playerID: socket.playerID,
+          gameID: socket.games[game]
+        })
+      }
+  })
 
   socket.on('join-game', function (data) {
 
@@ -180,7 +221,6 @@ io.on('connection', function (socket) {
     var update = {}
     update[data.playerID] = {
       connected: true,
-      sid: socket.id
     }
     walkoff.table('games').get(data.gameID).
     update(update).run(connection, function(err, response) {})
@@ -224,20 +264,6 @@ io.on('connection', function (socket) {
     })
   })
 
-  socket.on('send-powerup', function (data) {
-    socket.to(data.gameID).emit('powerup-used', {
-      powerup: data.powerUpID,
-      to: data.powerUpTarget,
-      from: data.playerID
-    })
-  })
-
-  socket.on('get-rooms', function () {
-    socket.emit('room-list', {
-      rooms: socket.rooms
-    })
-  })
-
   socket.on('get-player-id', function () {
     socket.emit('player-id', {
       playerID: socket.playerID
@@ -248,19 +274,6 @@ io.on('connection', function (socket) {
    * Send a player disconnection notice to all other players in the game
    * data required: none
    */
-  socket.on('disconnect', function() {
-    console.log('\n' + getTimeStamp() + ' player disconnected (this message could be delayed)' +
-                '\n\tsid: ' + socket.id +
-                '\n\tplayerID: ' + socket.playerID +
-                '\n\tleaving games:')
-    for (game in socket.games) {
-      console.log('\t' + socket.games[game])
-      socket.to(socket.games[game]).emit('player-disconnected', {
-          playerID: socket.playerID,
-          gameID: socket.games[game]
-        })
-      }
-  })
 })
 
 server.listen(2000)
@@ -268,5 +281,15 @@ console.info(getTimeStamp() + ' walkoff-server started. Listening on port 2000.'
 
 function getTimeStamp() {
   var date = new Date()
-  return date.getHours() + ':' + date.getMinutes() +':' + date.getSeconds()
+  return reformat(date.getHours()) + ':' +
+ 	 reformat(date.getMinutes()) + ':' +
+	 reformat(date.getSeconds())
+
+  function reformat(number) {
+    if (number < 10) {
+      var reformattedNumber = '0' + number
+      return reformattedNumber
+    } else { return number }
+  }  
 }
+
