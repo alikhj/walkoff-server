d19311b299cdf2bd2241409c6439c86b67924af4
Ali Khawaja <alikhj@gmail.com>
2015-09-10 23:44:13 -0400

    modularize 3

---
+++ socketHandlers/disconnect/disconnect.js
@@ -0,0 +1,39 @@
+var r = require('../../setupDatabase'),
+  rethink = require('rethinkdb'),
+  getTimeStamp = require('../../helpers/getTimeStamp')
+
+module.exports = function disconnect(socket) {
+  r.db.table('players').filter({
+    sid: socket.id
+  }).update({ connected: false }, {returnChanges: true}).
+
+  run(r.connection, function(err, response) {
+    if (typeof response.changes === 'undefined') { console.log('browser disconnected'); return }
+    if (response.changes[0]) { 
+      var player = response.changes[0].new_val
+      callback(player)
+    }
+    else {
+      //error handling if socket id doesn't exist
+    }
+
+  })
+
+  function callback(player) {
+    var playerID = player.id
+    var games = player.games
+      
+    console.log(getTimeStamp() + playerID + ' disconnected')
+      
+    for(var i = 0; i < games.length; i++) {
+      var gameID = games[i]
+      
+      socket.to(gameID).emit('player-disconnected', {
+        playerID: playerID,
+        gameID: gameID
+      })
+
+      console.log(getTimeStamp() + playerID + ' was disconnected from ' + gameID)
+    }
+  }
+}
--- socketHandlers/playerConnected/playerConnected.js
+++ socketHandlers/playerConnected/playerConnected.js
@@ -2,33 +2,30 @@ var r = require('../../setupDatabase'),
   rethink = require('rethinkdb'),
   getTimeStamp = require('../../helpers/getTimeStamp')
 
-module.exports = function playerConnected(socket) {
+module.exports = function playerConnected(socket, socketData) {
 
-  socket.on('player-connected', function(socketData) {
+  var update = {
+    sid: socket.id,
+    connected: true
+  }
 
-    var update = {
-      sid: socket.id,
-      connected: true
-    }
-
-    r.db.table('players').get(socketData.playerID).update(update).
-    run(r.connection, function(err, response) {
-      callback(response)
-    })
-
-    function callback(response) {
-
-      if (response.skipped == 1) {
-        var createNewPlayer =
-        require('./createNewPlayer')(socket, socketData)
-      }
-
-      //if player already exists
-      else {
-        var updateExistingPlayer =
-        require('./updateExistingPlayer')(socket, socketData)
-
-      }
-    }
+  r.db.table('players').get(socketData.playerID).update(update).
+  run(r.connection, function(err, response) {
+    callback(response)
   })
+
+  function callback(response) {
+
+    if (response.skipped == 1) {
+      var createNewPlayer =
+      require('./createNewPlayer')(socket, socketData)
+    }
+
+    //if player already exists
+    else {
+      var updateExistingPlayer =
+      require('./updateExistingPlayer')(socket, socketData)
+
+    }
+  }
 }
